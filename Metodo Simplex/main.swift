d30212d395900a4e6a3dff4287b07b6ccb2b112d
Leonardo <leonardo_rock@icloud.com>
2018-04-09 23:01:04 -0300

    Initial Commit

---
+++ Metodo Simplex/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Metodo Simplex
+//
+//  Created by Leonardo Oliveira on 4/9/18.
+//  Copyright © 2018 Leonardo Oliveira. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
